e18bb9cf5dda538a967545ab81b45729e46de80a
ekeneakubue <ekeneakubue@gmail.com>
2025-12-29 11:23:47 +0100

    fix all implicit any types for build

--- debug_read_schema.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('prisma/schema.prisma', 'utf8');
-console.log('--- Content Start ---');
-console.log(content);
-console.log('--- Content End ---');
-console.log('Length:', content.length);
-console.log('First 100 chars (hex):', Buffer.from(content.substring(0, 100)).toString('hex'));
